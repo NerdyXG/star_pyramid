57f18c1c536024c21afbe2ce8f44de6efe5fa996
= <108344292+NerdyXG@users.noreply.github.com>
2023-12-28 15:26:41 +0100

    Allow user interaction

--- pyramid.py
+++ pyramid.py
@@ -17,11 +17,52 @@
 
 def print_stars(n):
 	whitespace = 0
-	for i in range(n, 0, -2):
+	for i in range(n, 0, step):
 		print(whitespace * " ", end="")
 		for j in range(i):
 			print("*", end=" ")
-		whitespace += 2
+		whitespace += num_of_whitespace
 		print()
 
-print_stars(7)
+
+print("n = the maximum number of stars in a row, usually the first row")
+print("step = by how many digits should the stars decrease")
+print("floating point numbers are rounded down to their nearest int")
+print("------------------------")
+
+
+
+# validates the value of n
+while True:
+	try:
+		n = int(input("n = "))
+	except ValueError:
+		print("Non-numeric symbols are not supported!")
+	else:
+		if n < 1:
+			print("n must be a number greater than zero (0)")
+		else:
+			break
+
+# validates the step value
+while True:
+	try:
+		step = int(input("step = "))
+	except ValueError:
+		print("Non-numeric symbols are not supported!")
+	else:
+		if step >= n:
+			print(f"Step must be lesser than n, {n}...")
+		else:
+			break
+
+
+# ensures that step is negative
+if step > 0:
+	num_of_whitespace = step
+	step = 0 - step
+
+else:
+	num_of_whitespace = 0 - step
+
+print_stars(n)
